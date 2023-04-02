3d5c532c6415c22f3e9a999c5f747fe2a8e600d8
b1129006 <120005503+b1129006@users.noreply.github.com>
2023-04-02 13:25:21 +0800

    Add files via upload

---
+++ hw1/main6.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(){
+    char a[] = "81c8g8u168";
+    for (int i = 0; i < sizeof(a)-1; i++){
+        if (a[i] == '1'){
+            printf("I ");
+        }else if (a[i] == '2'){
+            printf("II ");
+        }else if (a[i] == '3'){
+            printf("III ");
+        }else if (a[i] == '4'){
+            printf("IV ");
+        }else if (a[i] == '5'){
+            printf("V ");
+        }else if (a[i] == '6'){
+            printf("VI ");
+        }else if (a[i] == '7'){
+            printf("VII ");
+        }else if (a[i] == '8'){
+            printf("VIII ");
+        }else if (a[i] == '9'){
+            printf("IX ");
+        }else {
+            printf("%c", a[i]);
+        }
+    }
+    
+    return 0;
+}
