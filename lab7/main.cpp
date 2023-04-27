e2632d83ed01f651cd008f12d9ec1e5f3a9f5aff
b1129006 <120005503+b1129006@users.noreply.github.com>
2023-04-27 14:27:12 +0800

    Add files via upload

---
+++ lab7/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+class Car{
+    public:
+    string m_brand;
+    string m_model;
+    int m_year;
+    int m_MaxSeating;
+    Car(string x, string y, int z, int s){
+        m_brand = x;
+        m_model = y;
+        m_year = z;
+        m_MaxSeating;
+    }
+    int get_m_MaxSeating(){
+        return m_MaxSeating;
+    }
+};
+class BMW_Car : public Car{
+    private:
+    string m_DriveMode;
+    
+    public:
+    BMW_Car(string y, int z, int s) : Car("BMW", y, z, s){
+        cout << "Coustructing BMW_Car\n";
+        m_DriveMode = "Rear-wheel";
+    }
+    
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+};
+
+class AUDI_Car : public Car{
+    private:
+    string m_DriveMode;
+    
+    public:
+    AUDI_Car(string y, int z, int s) : Car("AUDI", y, z, s){
+        cout << "Coustructing AUDI_Car\n";
+        m_DriveMode = "Front-wheel";
+    }
+    
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+};
+
+class BENZ_Car : public Car{
+    private:
+    string m_DriveMode;
+    
+    public:
+    BENZ_Car(string y, int z, int s) : Car("BENZ", y, z, s){
+        cout << "Coustructing BENZ_Car\n";
+        m_DriveMode = "Front-wheel";
+    }
+    
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+};
+
+
+int main(){
+    BMW_Car car_1("X5", 2023 , 6);
+    cout << car_1.m_brand;
+    cout << " : Drive Mode = " << car_1.get_DriveMode() << endl;
+    
+    AUDI_Car car_2("A1", 2023, 5);
+    cout << car_2.m_brand;
+    cout << " : Drive Mode =" << car_2.get_DriveMode() << endl;
+    
+    BENZ_Car car_3("JL", 2023, 7);
+    cout << car_3.m_brand;
+    cout << " : Drive Mode =" << car_3.get_DriveMode() << endl;
+    
+}
